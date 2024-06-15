dde4f992506acfc16f128ebea536fdf654e226ca
Vadim Shikhovtsov <vadimx30@gmail.com>
2024-06-15 14:08:03 +0300

    Add comments. In process ...

--- MainApp/models.py
+++ MainApp/models.py
@@ -11,6 +11,9 @@ LANGS = (
 
 
 class Snippet(models.Model):
+    class Meta:
+        ordering = ["name", "lang"]
+         
     name = models.CharField(max_length=100)
     lang = models.CharField(max_length=30, choices=LANGS)
     code = models.TextField(max_length=5000)
@@ -19,4 +22,14 @@ class Snippet(models.Model):
     public = models.BooleanField(default=True)  # True = public, False = private
 
     def __repr__(self) -> str:
-        return f"Snippet({self.name})"
+        return f"Snippet({self.name}, {self.lang})"
+    
+
+class Comment(models.Model):
+    text = models.TextField(max_length=1000)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    snippet = models.ForeignKey(to=Snippet, on_delete=models.CASCADE, related_name='comments')
+
+    
+
--- MainApp/views.py
+++ MainApp/views.py
@@ -49,7 +49,8 @@ def snippets_page(request):
     snippets = Snippet.objects.filter(public=True)
     context = {
         'pagename': 'Просмотр сниппетов',
-        'snippets': snippets
+        'snippets': snippets,
+        'count': snippets.count()
         }
     return render(request, 'pages/view_snippets.html', context)
 

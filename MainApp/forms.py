e86a750efa07fd964f5487d312510e9e1c9f4a10
sanches <sanchessnz@gmail.com>
2024-11-29 16:32:30 +0500

    login and comment done

--- MainApp/forms.py
+++ MainApp/forms.py
@@ -1,13 +1,16 @@
 from django.forms import ModelForm, ValidationError, TextInput, Textarea
-from MainApp.models import Snippet
+from MainApp.models import Comment, Snippet
+from django.contrib.auth.models import User
+from django.forms import CharField, PasswordInput
+from django.core.exceptions import ValidationError
 
 
 class SnippetForm(ModelForm):
     class Meta:
         model = Snippet
         # Описываем поля, которые будем заполнять в форме
-        fields = ['name', 'lang', 'code']
-        labels = {"name": "", "lang": "", "code": ""}
+        fields = ['name', 'lang', 'code', 'public']
+        labels = {"name": "", "lang": "", "code": "", "public": ""}
         widgets = {
             "name": TextInput(attrs={"placeholder": "Название сниппета"}),
             "code": Textarea(attrs={"placeholder": "Код сниппета"})
@@ -18,3 +21,42 @@ class SnippetForm(ModelForm):
         if len(snippet_name) > 3:
             return snippet_name
         raise ValidationError("snippet name too short")
+
+
+class UserRegistrationForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ["username", "email"]
+
+    password1 = CharField(label="password", widget=PasswordInput)
+    password2 = CharField(label="password confirm", widget=PasswordInput)
+
+    def clean_username(self):
+        username = self.cleaned_data.get("username")
+        if len(username) > 3:
+            return username
+        raise ValidationError("Username too short!")
+
+    def clean_password2(self):
+        pass1 = self.cleaned_data.get("password1")
+        pass2 = self.cleaned_data.get("password2")
+        if pass1 and pass2 and pass1 == pass2:
+            return pass2
+        raise ValidationError("Пароли не совпадают или пустые")
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class CommentForm(ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["text"]
+        labels = {"text": ""}
+        widgets = {
+            "text": Textarea(attrs={"placeholder": "Комментарий для сниппета"})
+        }
